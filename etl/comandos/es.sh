6ce3ccfaba4f0661ec00e1f7a75051d3fecc3a32
Thiago Oliveira <thiagok2@gmail.com>
2019-05-13 14:05:24 -0300

    SP, AL, ES

---
+++ etl/comandos/es.sh
@@ -0,0 +1,2 @@
+cd /home/thiago/desenv/pentaho/pdi-ce-8.2.0.0-342/data-integration
+./kitchen.sh /file /home/thiago/desenv/Normativas/normativas-crawlers-etl/etl/ES.kjb "/param:DIR_UPLOAD=/var/www/normativas-v02/storage/app/public/uploads" "/param:URL_BASE=http://normativas-v02.com/api/documentos/"
---
+++ etl/comandos/sp.sh
@@ -0,0 +1,2 @@
+cd /home/thiago/desenv/pentaho/pdi-ce-8.2.0.0-342/data-integration
+./kitchen.sh /file /home/thiago/desenv/Normativas/normativas-crawlers-etl/etl/SP.kjb
