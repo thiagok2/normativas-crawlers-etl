3200bb5fe44bac6dc61a3ebcaced61e9e5c6589b
Thiago Oliveira <thiagok2@gmail.com>
2019-05-15 13:01:08 -0300

    add clear env

---
+++ etl/comandos/clear.sh
@@ -0,0 +1,3 @@
+
+cd /home/thiago/desenv/pentaho/pdi-ce-8.2.0.0-342/data-integration
+./pan.sh /file /home/thiago/desenv/Normativas/normativas-crawlers-etl/etl/clearEnv.ktr
--- etl/comandos/teste.sh
+++
@@ -1,5 +0,0 @@
-export DIR_UPLOAD="/var/www/normativas-v02/storage/app/public/uploads"
-export URL_BASE="http://normativas-v02.com/api/documentos/"
-
-cd /home/thiago/desenv/pentaho/pdi-ce-8.2.0.0-342/data-integration
-./kitchen.sh /file /home/thiago/desenv/Normativas/normativas-crawlers-etl/etl/AL.kjb "/param:DIR_UPLOAD=/var/www/normativas-v02/storage/app/public/uploads" "/param:URL_BASE=http://normativas-v02.com/api/documentos/"
