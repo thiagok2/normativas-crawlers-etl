80a9460a001bf267a23041f3b5f5d80345a98e96
Thiago Oliveira <thiagok2@gmail.com>
2019-06-12 22:24:01 -0300

    etl RO

---
+++ etl/comandos/ro.sh
@@ -0,0 +1,6 @@
+#export PDI_ROOT="/home/thiago/desenv/pentaho/pdi-ce-8.2.0.0-342/data-integration"
+#export ETL_ROOT="/home/thiago/desenv/Normativas/normativas-crawlers-etl/etl"
+
+
+cd $PDI_ROOT
+./kitchen.sh /file $ETL_ROOT/RO.kjb
